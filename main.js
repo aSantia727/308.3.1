425f0d6a1fb17d075355f38d8f00940d3b26343f
asantia727 <asantia727@gmail.com>
2024-09-24 12:15:10 -0400

    message

---
+++ main.js
@@ -0,0 +1,21 @@
+// //Part 1: Fizz-Buzz 
+
+// for (let i = 0; i <= 100; i++){
+//     if (i % 15 == 0)
+//         console.log("Fizz Buzz")
+//     else if ((i % 3==0) && (i % 5 != 0))
+//         console.log("Fizz")
+//     else if ((i % 3 != 0) && (i % 5 == 0))
+//         console.log("Buzz")
+// }
+
+//Part 2: Prime Time
+let i = 8;
+
+for (i = i+1; i < 99; i++)
+    if ((i % 2 != 0) && (i % 3 != 0)){
+        console.log(i)
+        break;
+} 
+
+//Part 3: Feeling Loopy
